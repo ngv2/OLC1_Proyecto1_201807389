d1a7cf72bdd37dbfe893369e1c96eabafba7d625
lets <3009044520101@ingenieria.usac.edu.gt>
2024-05-02 20:25:19 -0600

    [ADD] Funcionamiento proyecto 1.
    [ADD] Funcionamiento proyecto 1.

---
+++ src/ErrToken.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tokens;
+
+/**
+ *
+ * @author lets
+ */
+public class ErrToken {
+    
+    private String lexema;
+    private int fila;
+    private int columna;
+
+    public ErrToken(String lexema, int fila, int columna) {
+        this.lexema = lexema;
+        this.fila = fila;
+        this.columna = columna;
+    }
+
+    public String getLexema() {
+        return lexema;
+    }
+
+    public void setLexema(String lexema) {
+        this.lexema = lexema;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        this.fila = fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+    
+}
---
+++ src/Metodos/Analisis.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Metodos;
+
+import analizador.Parser;
+import java.io.StringReader;
+
+/**
+ *
+ * @author lets
+ */
+public class Analisis {
+    public void analizadores(String ruta, String jflexFile, String cupFile){
+        try {
+            String opcionesJflex[] =  {ruta+jflexFile,"-d",ruta};
+            jflex.Main.generate(opcionesJflex);
+
+            String opcionesCup[] =  {"-destdir", ruta,"-parser","Parser",ruta+cupFile};
+            java_cup.Main.main(opcionesCup);
+            
+        } catch (Exception e) {
+            System.out.println("No se ha podido generar los analizadores");
+            System.out.println(e);
+        }
+    }
+        // Realizar Analisis
+    public void analizar (String entrada){
+        try {
+            analizador.Lexer lexer = new analizador.Lexer(new StringReader(entrada)); 
+            analizador.Parser parser = new Parser(lexer);
+            parser.parse();
+        } catch (Exception e) {
+            System.out.println("Error fatal en compilación de entrada.");
+            System.out.println(e);
+        } 
+    } 
+}
